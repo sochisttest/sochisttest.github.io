f5cc659ce03b1669843988fb6da97155cbecff59
sochisttest <142121345+sochisttest@users.noreply.github.com>
2023-08-13 07:34:27 -0700

    Add hidden neutrality value

--- ideologies.js
+++ ideologies.js
@@ -1,7 +1,9 @@
 ideologies = [
     {
-        "name": "Agrarian Socialism (Narodniks/Socialist Revolutionaries)",
+        "name": "Agrarian Socialism",
+        "orgs": "Russian Socialist Revolutionary Party",
         "stats": {
+            "neut": 0,
             "prol": 0,
             "stat": 50,
             "econ": 50,
@@ -16,24 +18,10 @@ ideologies = [
         }
     },
     {
-        "name": "Agrarian Anarchism (Makhnovshchina)",
-        "stats": {
-            "prol": 0,
-            "stat": 0,
-            "econ": 50,
-            "prty": 10,
-            "intl": 70,
-            "bols": 10,
-            "lcom": 50,
-            "stal": 50,
-            "ussr": 0,
-            "coop": 50,
-            "radc": 50,
-        }
-    },
-    {
         "name": "Anarchist Collectivism (Bakuninism)",
+        "orgs": "",
         "stats": {
+            "neut": 0,
             "prol": 50,
             "stat": 0,
             "econ": 50,
@@ -48,13 +36,15 @@ ideologies = [
         }
     },
     {
-        "name": "Anarcho-Communism",
+        "name": "Anarcho-Communism (Kropotkinism)",
+        "orgs": "Makhnovshchina",
         "stats": {
+            "neut": 0,
             "prol": 50,
             "stat": 0,
             "econ": 50,
-            "prty": 0,
-            "intl": 50,
+            "prty": 10,
+            "intl": 70,
             "bols": 50,
             "lcom": 100,
             "stal": 50,
@@ -64,8 +54,28 @@ ideologies = [
         }
     },
     {
-        "name": "Industrial Unionism (Syndicalism)",
+        "name": "Insurrectionary Anarchism",
+        "orgs": "",
         "stats": {
+            "neut": 0,
+            "prol": 0,
+            "stat": 0,
+            "econ": 50,
+            "prty": 0,
+            "intl": 50,
+            "bols": 50,
+            "lcom": 100,
+            "stal": 50,
+            "ussr": 50,
+            "coop": 10,
+            "radc": 100,
+        }
+    },
+    {
+        "name": "Industrial Unionism (Syndicalism)",
+        "orgs": "Industrial Workers of the World",
+        "stats": {
+            "neut": 50,
             "prol": 70,
             "stat": 40,
             "econ": 100,
@@ -73,7 +83,7 @@ ideologies = [
             "intl": 70,
             "bols": 50,
             "lcom": 50,
-            "stal": 50,
+            "stal": 60,
             "ussr": 50,
             "coop": 50,
             "radc": 40,
@@ -81,12 +91,14 @@ ideologies = [
     },
     {
         "name": "Anarcho-Syndicalism",
+        "orgs": "CNT-FAI",
         "stats": {
+            "neut": 0,
             "prol": 70,
             "stat": 0,
             "econ": 100,
             "prty": 30,
-            "intl": 50,
+            "intl": 80,
             "bols": 50,
             "lcom": 50,
             "stal": 50,
@@ -97,7 +109,9 @@ ideologies = [
     },
     {
         "name": "Postcolonial Anarchism",
+        "orgs": "",
         "stats": {
+            "neut": 0,
             "prol": 30,
             "stat": 0,
             "econ": 50,
@@ -113,7 +127,9 @@ ideologies = [
     },
     {
         "name": "Classical Marxism",
+        "orgs": "",
         "stats": {
+            "neut": 50,
             "prol": 100,
             "stat": 33,
             "econ": 0,
@@ -129,7 +145,9 @@ ideologies = [
     },
     {
         "name": "Classical Leninism",
+        "orgs": "Bolshevik Party",
         "stats": {
+            "neut": 50,
             "prol": 90,
             "stat": 33,
             "econ": 0,
@@ -145,9 +163,11 @@ ideologies = [
     },
     {
         "name": "Kautskyism (Centrist Marxism)",
+        "orgs": "Social Democratic Party of Germany (Pre-WW1)",
         "stats": {
+            "neut": 0,
             "prol": 100,
-            "stat": 33,
+            "stat": 40,
             "econ": 20,
             "prty": 100,
             "intl": 80,
@@ -160,16 +180,36 @@ ideologies = [
         }
     },
     {
-        "name": "Orthodox Trotskyism",
+        "name": "Neo-Kautskyism",
+        "orgs": "CPGB (PCC)",
         "stats": {
+            "neut": 0,
+            "prol": 100,
+            "stat": 40,
+            "econ": 20,
+            "prty": 100,
+            "intl": 80,
+            "bols": 50,
+            "lcom": 30,
+            "stal": 60,
+            "ussr": 20,
+            "coop": 80,
+            "radc": 80,
+        }
+    },
+    {
+        "name": "Orthodox Trotskyism",
+        "orgs": "International Committee of the 4th International",
+        "stats": {
+            "neut": 0,
             "prol": 100,
             "stat": 33,
-            "econ": 0,
-            "prty": 100,
+            "econ": 20,
+            "prty": 80,
             "intl": 100,
             "bols": 100,
             "lcom": 50,
-            "stal": 0,
+            "stal": 10,
             "ussr": 60,
             "coop": 40,
             "radc": 100,
@@ -177,15 +217,17 @@ ideologies = [
     },
     {
         "name": "Entryist Trotskyism (Pabloism/Posadism)",
+        "orgs": "International Secretariat of the 4th International",
         "stats": {
+            "neut": 0,
             "prol": 100,
             "stat": 33,
-            "econ": 0,
-            "prty": 100,
-            "intl": 100,
+            "econ": 20,
+            "prty": 80,
+            "intl": 90,
             "bols": 100,
             "lcom": 50,
-            "stal": 0,
+            "stal": 10,
             "ussr": 80,
             "coop": 70,
             "radc": 100,
@@ -193,15 +235,17 @@ ideologies = [
     },
     {
         "name": "Third Camp Trotskyism (Schachtmanism/Cliffism)",
+        "orgs": "Worker's Party/Independent Socialist League",
         "stats": {
+            "neut": 0,
             "prol": 100,
             "stat": 33,
             "econ": 20,
-            "prty": 100,
+            "prty": 80,
             "intl": 100,
             "bols": 100,
             "lcom": 50,
-            "stal": 0,
+            "stal": 20,
             "ussr": 20,
             "coop": 40,
             "radc": 100,
@@ -209,7 +253,9 @@ ideologies = [
     },
     {
         "name": "Post-Trotskyist Anarchism (Taborites)",
+        "orgs": "The Utopian",
         "stats": {
+            "neut": 0,
             "prol": 80,
             "stat": 20,
             "econ": 20,
@@ -224,16 +270,18 @@ ideologies = [
         }
     },
     {
-        "name": "Communalism (Bookchin)", //Doesn't really belong here, but no eco category so wtv
+        "name": "Communalism/Democratic Confederalism", //Doesn't really belong here, but no eco category so wtv
+        "orgs": "Kurdistan Workers' Party",
         "stats": {
+            "neut": 0,
             "prol": 10,
             "stat": 10,
-            "econ": 0,
+            "econ": 20,
             "prty": 30,
-            "intl": 80,
+            "intl": 30,
             "bols": 50,
             "lcom": 50,
-            "stal": 10,
+            "stal": 30,
             "ussr": 50,
             "coop": 50,
             "radc": 100,
@@ -241,15 +289,17 @@ ideologies = [
     },
     {
         "name": "Marxist Humanism",
+        "orgs": "Marxist Humanist Initiative",
         "stats": {
+            "neut": 0,
             "prol": 80,
             "stat": 20,
-            "econ": 0,
+            "econ": 20,
             "prty": 50,
             "intl": 100,
             "bols": 60,
             "lcom": 70,
-            "stal": 0,
+            "stal": 10,
             "ussr": 0,
             "coop": 30,
             "radc": 100,
@@ -257,15 +307,17 @@ ideologies = [
     },
     {
         "name": "Left-Schachtmanism (Draperites)",
+        "orgs": "International Socialist Organization",
         "stats": {
+            "neut": 0,
             "prol": 90,
             "stat": 33,
-            "econ": 0,
+            "econ": 20,
             "prty": 75,
             "intl": 100,
             "bols": 90,
             "lcom": 70,
-            "stal": 10,
+            "stal": 20,
             "ussr": 10,
             "coop": 30,
             "radc": 100,
@@ -273,7 +325,9 @@ ideologies = [
     },
     {
         "name": "Trade Unionism",
+        "orgs": "AFL-CIO",
         "stats": {
+            "neut": 100,
             "prol": 60,
             "stat": 50,
             "econ": 100,
@@ -289,7 +343,9 @@ ideologies = [
     },
     {
         "name": "Progressivism (Social Liberalism/Post-war Social Democracy)",
+        "orgs": "Social Democratic Party of Germany (Post-WW2)",
         "stats": {
+            "neut": 50,
             "prol": 50,
             "stat": 80,
             "econ": 20,
@@ -305,7 +361,9 @@ ideologies = [
     },
     {
         "name": "Big-Tent Democratic Socialism (Harringtonism)",
+        "orgs": "Democratic Socialists of America",
         "stats": {
+            "neut": 100,
             "prol": 50,
             "stat": 50,
             "econ": 30,
@@ -321,7 +379,9 @@ ideologies = [
     },
     {
         "name": "Contemporary Class-first Socialism",
+        "orgs": "",
         "stats": {
+            "neut": 100,
             "prol": 80,
             "stat": 50,
             "econ": 60,
@@ -337,7 +397,9 @@ ideologies = [
     },
     {
         "name": "Contemporary Intersectional Leftism",
+        "orgs": "",
         "stats": {
+            "neut": 100,
             "prol": 30,
             "stat": 50,
             "econ": 0,
@@ -353,7 +415,9 @@ ideologies = [
     },
     {
         "name": "Marxism-Leninism (Stalinism)",
+        "orgs": "Communist Party of the Soviet Union",
         "stats": {
+            "neut": 0,
             "prol": 70,
             "stat": 50,
             "econ": 40,
@@ -369,7 +433,9 @@ ideologies = [
     },
     {
         "name": "Anti-Revisionist Marxism-Leninism (Stalinism/Hoxhaism)",
+        "orgs": "Party of Labour of Albania",
         "stats": {
+            "neut": 0,
             "prol": 70,
             "stat": 50,
             "econ": 40,
@@ -385,7 +451,9 @@ ideologies = [
     },
     {
         "name": "Titoism",
+        "orgs": "League of Communists of Yugoslavia",
         "stats": {
+            "neut": 0,
             "prol": 70,
             "stat": 50,
             "econ": 40,
@@ -401,7 +469,9 @@ ideologies = [
     },
     {
         "name": "Mao Zedong Thought",
+        "orgs": "Communist Party of China (Pre-Deng)",
         "stats": {
+            "neut": 0,
             "prol": 50,
             "stat": 50,
             "econ": 60,
@@ -417,7 +487,9 @@ ideologies = [
     },
     {
         "name": "Marxism-Leninism-Maoism",
+        "orgs": "Revolutionary Internationalist Movement",
         "stats": {
+            "neut": 0,
             "prol": 50,
             "stat": 50,
             "econ": 40,
@@ -433,7 +505,9 @@ ideologies = [
     },
     {
         "name": "Deng Xiaoping Thought",
+        "orgs": "Communist Party of China (Post-Mao)",
         "stats": {
+            "neut": 0,
             "prol": 50,
             "stat": 60,
             "econ": 40,
@@ -449,7 +523,9 @@ ideologies = [
     },
     {
         "name": "Right-Communism (Bukharinism)",
+        "orgs": "Russian Communist Party (Right Opposition)",
         "stats": {
+            "neut": 0,
             "prol": 80,
             "stat": 33,
             "econ": 20,
@@ -465,7 +541,9 @@ ideologies = [
     },
     {
         "name": "Autonomist Marxism",
+        "orgs": "",
         "stats": {
+            "neut": 0,
             "prol": 70,
             "stat": 25,
             "econ": 20,
@@ -481,7 +559,9 @@ ideologies = [
     },
     {
         "name": "Left-Communism (Council Communism)",
+        "orgs": "Communist Workers' Party of Germany",
         "stats": {
+            "neut": 0,
             "prol": 100,
             "stat": 30,
             "econ": 20,
@@ -497,7 +577,9 @@ ideologies = [
     },
     {
         "name": "Bolshevik Left Opposition",
+        "orgs": "Russian Communist Party (Left Opposition)",
         "stats": {
+            "neut": 0,
             "prol": 100,
             "stat": 33,
             "econ": 20,
@@ -513,15 +595,17 @@ ideologies = [
     },
     {
         "name": "Eurocommunism",
+        "orgs": "",
         "stats": {
+            "neut": 30,
             "prol": 70,
-            "stat": 50,
+            "stat": 60,
             "econ": 40,
-            "prty": 80,
+            "prty": 70,
             "intl": 50,
             "bols": 50,
             "lcom": 40,
-            "stal": 40,
+            "stal": 50,
             "ussr": 0,
             "coop": 80,
             "radc": 50,
@@ -529,7 +613,9 @@ ideologies = [
     },
     {
         "name": "Reformist Marxism (Bernsteinian Social Democracy)",
+        "orgs": "Independent Social Democratic Party of Germany",
         "stats": {
+            "neut": 0,
             "prol": 70,
             "stat": 60,
             "econ": 50,
